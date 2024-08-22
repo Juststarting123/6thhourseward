f75042eeada7a7be253f66c9b96c48b0d47e2ac1
seeley seward <ss127040@student.wpsok.org>
2024-08-22 13:37:17 -0500

    added hw2

---
+++ HW2.py
@@ -0,0 +1,13 @@
+#Name:Seeley Seward
+#class: 6th hour
+#Assignment: HW2
+
+
+print("hello world")
+name = input("what is your name?")
+
+print(name)
+print("Hello",name)
+x = int(input("give me a number"))
+y = int(input("give me another"))
+print(x + y)
