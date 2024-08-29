f6a63cd1151359d8f0f20c2b65c3e3bbd7baca7d
seeley seward <ss127040@student.wpsok.org>
2024-08-29 13:48:31 -0500

    added hw3

---
+++ HM3.py
@@ -0,0 +1,27 @@
+#Name: Seeley seward
+#Class: hour 6
+#Assinment: HM3
+
+
+print("hello world")
+
+namelist=["Seeley","Evan","Coach Mack","Joe","Logan"]
+
+
+namelist.append("Bob")
+
+print(namelist[3])
+
+numberlist=[2, 3, 4, 5]
+
+numberlist.insert(1,6)
+
+print(numberlist)
+
+numberlist.sort()
+
+print(numberlist)
+
+sum = numberlist[0] + numberlist[1] + numberlist[2]
+
+print(sum)
