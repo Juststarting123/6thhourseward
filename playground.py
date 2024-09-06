eae2c4142f84f175598daa3d1ea5610a1ccbe195
seeley seward <ss127040@student.wpsok.org>
2024-09-06 13:56:44 -0500

    added playground

---
+++ playground.py
@@ -0,0 +1,88 @@
+print("hello")
+
+name = input("who are you?")
+
+name = input("what are you doing?")
+
+print("ok")
+
+print("Why?")
+
+name = input("What are you coding and what for?")
+
+print("oh")
+
+name = input("Hey! Wanna play a game?")
+
+print("YAY! :)")
+
+y = int(input("Ok pick a number, any number."))
+
+print("Alright")
+
+x = int(input("Now give me another number."))
+
+print("cool")
+
+print("Now add them both together.")
+
+print(x+y)
+
+print(":)")
+
+print("Hey! Wanna hear a fact I learned?")
+
+name = input("Do ya?")
+
+print("YAY! :D")
+
+print("Ketchup was once sold as medicine!")
+
+name = input("Can you believe that?")
+
+print("Me either...")
+
+print("Well,see you later!")
+
+name = input("bye!")
+
+print("oh...wait")
+
+print("I don't have legs")
+
+print(":(")
+
+print("Eh...oh well!")
+
+print("I'll live")
+
+print("oh hey!")
+
+print("wanna help me with something?")
+
+print("I'm making a list for something...")
+
+print("I have these ingredients.")
+
+namelist = ["bread","meat","cheese"]
+
+print(namelist[0])
+print(namelist[1])
+print("and")
+print(namelist[2])
+
+print("I wanna make a sandwich")
+
+name = input("But I wanna add another ingredient,got ideas?")
+
+print("Ok")
+
+print("Yay! My sandwich is ready!")
+
+print("Oh wait...")
+
+print("I can't eat it.:(")
+
+print("You can have it then. :)")
+
+name = input("ok,bye!")
