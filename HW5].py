cbd954b46a035984c69331db52dc3948239e3fff
seeley seward <ss127040@student.wpsok.org>
2024-09-27 13:52:22 -0500

    added HW5

---
+++ HW5].py
@@ -0,0 +1,16 @@
+#name: Seeley seward
+#Class: 6th hour
+#Assignment:HW5
+
+
+print("hello world")
+
+nublist=[2,4,6]
+
+if nublist[0]>nublist[1] and nublist[0]>nublist[2]:
+    print("A is the biggest number")
+elif nublist[1]>nublist[0] and nublist[1]>nublist[2]:
+    print("B is the biggest number")
+else:
+    print("C is the biggest number")
+
