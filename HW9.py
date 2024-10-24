dbe4e86af422455c57e4f07d8e9d1847630a2abe
seeley seward <ss127040@student.wpsok.org>
2024-10-24 13:43:46 -0500

    added HW9

---
+++ HW9.py
@@ -0,0 +1,18 @@
+#Name: Seeley Seward
+#Class: 6th Hour
+#Assignment: HW9
+
+#1. Print Hello World!
+#2. Go to the link below and convert the code into pseudocode in comments, then code the flowchart completely:
+print("hello world")
+
+temperature = 17
+if temperature > 20:
+    print("its hot")
+elif temperature > 10:
+        print("its mild")
+
+else:
+        print("it cold")
+
+print("thank you")
