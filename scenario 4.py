56cb27e1e1660fa0166da8f088f1e2acc17712ff
seeley seward <ss127040@student.wpsok.org>
2024-11-20 13:40:43 -0600

    added scenario 4

--- scenario 4.py
+++ scenario 4.py
@@ -10,6 +10,29 @@
 #They want to start prototyping the rating system and are asking you to make it.
 #This prototype needs to allow the user to input the number of players, let each player rate
 #a single model from 1 to 5, and then give the average score of all of the ratings.
+#Step 1. Establish Variables
 
-a = int(input())
+#Step 2. Make a loop
+
+#Step 2a. Ask for a rating number
+
+#Step 2b. Check if rating number is between 1 and 5
+
+#Step 2c. If rating number is valid, add to total
+
+#Step 3. Find and print the average
+playercount = int(input("insert number of players"))
+ratingsum = 0
+for i in range(1,playercount +1):
+
+    ratingnumb = int(input("please insert rating"))
+    if ratingnumb > 5 or ratingnumb < 1:
+        print("invalid number")
+    else:
+        ratingsum += ratingnumb
+
+Average = ratingsum / playercount
+print(Average)
+
+
 
